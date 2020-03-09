a8f1e7e0f2549213dceadd4a6524433830704ff1
lordofdestiny <djumicbranislav@outlook.com>
2020-03-09 22:55:29 +0100

    dist/ no longer included in project directory

--- dist/Utils/random.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const randomInt: (a: number, b: number) => number;
-export declare const randomBit: () => number;
-export declare const randomBitSequence: (length: number) => number[];
