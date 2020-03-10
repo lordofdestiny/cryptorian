027442d58c77412ff810a46a8464a3eb126384e6
lordofdestiny <djumicbranislav@outlook.com>
2020-03-10 11:56:00 +0100

    Updated tests for CeasarCipher class Bumped version

--- src/Ciphers/Ceasar/index.spec.ts
+++ src/Ciphers/Ceasar/index.spec.ts
@@ -1,35 +1,35 @@
 import CeasarCipher from "./index";
 import "../../jestModule";
 
-describe("Affine Cipher Class tests", () => {
-  test("It should throw for invalid a = %i", key => {
+describe("Ceasar Cipher Class tests", () => {
+  test.each([[-1], [""]])("It, should throw for invalid a = %i", key => {
     expect(() => {
-      new CeasarCipher(-1);
+      new CeasarCipher(key);
     }).toThrow();
   });
   test("Should create class instance", () => {
     expect(() => {
-      const affine = new CeasarCipher("text");
-      expect(affine).toBeInstanceOf(CeasarCipher);
+      const ceasar = new CeasarCipher("text");
+      expect(ceasar).toBeInstanceOf(CeasarCipher);
     });
   });
   test("Try getting and setting a key", () => {
-    const key = 5
-    const affine = new CeasarCipher(key);
-    expect(affine.key).toEqual(key);
+    const key = 5;
+    const ceasar = new CeasarCipher(key);
+    expect(ceasar.key).toEqual(key);
     const key2 = 31;
-    affine.key = key2;
-    expect(affine.key).toEqual(key2);
+    ceasar.key = key2;
+    expect(ceasar.key).toEqual(key2);
   });
   test("Check are keys equivalent", () => {
-    const affine = new CeasarCipher("text");
-    expect(affine.isEquivalentKey("T")).toEqual(true);
+    const ceasar = new CeasarCipher("text");
+    expect(ceasar.isEquivalentKey(19)).toEqual(true);
   });
   test("Encryption/decryption", () => {
-    const affine = new CeasarCipher("xd");
+    const ceasar = new CeasarCipher("xd");
     const text = "This is a text id like to encrypt".toUpperCase();
-    const encrypted = affine.encrypt(text);
-    const decrypted = affine.decrypt(encrypted);
+    const encrypted = ceasar.encrypt(text);
+    const decrypted = ceasar.decrypt(encrypted);
     expect(decrypted).toEqual(text);
   });
 });
--- src/Ciphers/Ceasar/index.ts
+++ src/Ciphers/Ceasar/index.ts
@@ -1,37 +1,37 @@
-import {isAlpha} from "../../Utils/other";
+import { isAlpha } from "../../Utils/other";
 import { KeyCipher } from "../../AbstractCiphers";
 
-type KeyCeasarCipher = number|string;
+type KeyCeasarCipher = number | string;
 
-export default class CeasarCipher extends KeyCipher{
-  private userKey : KeyCeasarCipher;
-  private workKey : number;
-  private static alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  constructor(key:KeyCeasarCipher) {
+export default class CeasarCipher extends KeyCipher {
+  private userKey: KeyCeasarCipher;
+  private workKey: number;
+  private static alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  constructor(key: KeyCeasarCipher) {
     super();
     CeasarCipher.checkKey(key);
     this.userKey = key;
     this.workKey = CeasarCipher.prepareKey(key);
   }
 
-  private static checkKey(key:KeyCeasarCipher){
-    if(typeof key == "number"){
-      if(key >= 0){
+  private static checkKey(key: KeyCeasarCipher) {
+    if (typeof key == "number") {
+      if (key < 0) {
         throw new Error("Key must not be less than 0");
       }
-    }else if(typeof key === "string"){
-      if(key.length < 1){
+    } else if (typeof key === "string") {
+      if (key.length < 1) {
         throw new Error("Key must 1 or more charachters long!");
-      }else if(!isAlpha(key)){
+      } else if (!isAlpha(key)) {
         throw new Error("Key can only contain letters");
       }
     }
   }
 
-  private static prepareKey(key:KeyCeasarCipher) {
-    if(typeof key == "number"){
+  private static prepareKey(key: KeyCeasarCipher) {
+    if (typeof key == "number") {
       return key % 26;
-    }else {
+    } else {
       return key[0].toUpperCase().charCodeAt(0) - 65;
     }
   }
@@ -46,8 +46,7 @@ export default class CeasarCipher extends KeyCipher{
     this.workKey = CeasarCipher.prepareKey(key);
   }
 
-
-  public encrypt(text:string) {
+  public encrypt(text: string) {
     return text
       .toUpperCase()
       .split("")
@@ -59,24 +58,25 @@ export default class CeasarCipher extends KeyCipher{
       .join("");
   }
 
-  public decrypt(text:string) {
+  public decrypt(text: string) {
     return text
       .toUpperCase()
       .split("")
       .map(char =>
         isAlpha(char)
-          ? CeasarCipher.alphabet[(char.charCodeAt(0) - 65 + 26 - this.workKey) % 26]
+          ? CeasarCipher.alphabet[
+              (char.charCodeAt(0) - 65 + 26 - this.workKey) % 26
+            ]
           : char
       )
       .join("");
   }
 
   public isEquivalentKey(key: KeyCeasarCipher): boolean {
-    if(typeof key == "number"){
+    if (typeof key == "number") {
       return this.workKey == key % 26;
-    }else{
-      return this.workKey == CeasarCipher.prepareKey(key)
+    } else {
+      return this.workKey == CeasarCipher.prepareKey(key);
     }
   }
-
 }
