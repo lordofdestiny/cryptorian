524a56b547a7e86798965faadf210d173589cd56
lordofdestiny <djumicbranislav@outlook.com>
2020-03-11 16:48:57 +0100

    Exported OneTimePad

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,17 @@ import { AffineCipher } from "./Ciphers/Affine/index";
 import { AtbashCipher } from "./Ciphers/Atbash/index";
 import { BaconianCipher } from "./Ciphers/Baconian/index";
 import { CeasarCipher } from "./Ciphers/Ceasar/index";
+import { OneTimePad } from "./Ciphers/OTP";
 
 import * as Random from "./Utils/random";
 
-const Ciphers = { AffineCipher, AtbashCipher, BaconianCipher, CeasarCipher };
+const Ciphers = {
+  AffineCipher,
+  AtbashCipher,
+  BaconianCipher,
+  CeasarCipher,
+  OneTimePad
+};
 const Utils = { Random };
 
 export { Ciphers, Utils };
