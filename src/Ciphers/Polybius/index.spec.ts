a9ae742441551f682aa11c02c952bf39bd874474
lordofdestiny <djumicbranislav@outlook.com>
2020-03-12 23:01:33 +0100

    Completly implemented Polyibues cipher in TS Optimized it completly

--- src/Ciphers/Polybius/index.spec.ts
+++ src/Ciphers/Polybius/index.spec.ts
@@ -2,23 +2,26 @@ import { PolybiusCipher } from "./index";
 import "../../jestModule";
 
 describe("Polybius Cipher", () => {
-  test.each([["123", "a1basdasfg4"]])("Should fail to initialize", key => {
+  test.each([
+    ["123", null, null],
+    ["abcde", "ABCD", null],
+    ["xywyz", "1234", null],
+    ["A345", false, "124"],
+    ["A345", true, "124"],
+    ["A41", false, "12345"]
+  ])("Should fail to init (%p,%p,%p)", (key: any, arg0: any, arg1: any) => {
     expect(() => {
-      new PolybiusCipher(key);
+      new PolybiusCipher(key, arg0, arg1);
     }).toThrow();
   });
-  test("Should check that encrypt returns", () => {
-    const cipher = new PolybiusCipher("thiskey");
+  test("Should check that encrypt/decrypt always ciphers well", () => {
+    const cipher = new PolybiusCipher("thiskey", true);
     const text = cipher.encrypt("abc1");
-
-    expect(text).toBeString();
+    expect(cipher.decrypt(text)).toEqual("abc1");
   });
-
-  test("Should check that decrypt returns", () => {
-    const cipher = new PolybiusCipher("thiskey");
-    const text = cipher.encrypt("abc");
-    const deced = cipher.decrypt(text);
-    expect(deced).toBeString();
-    expect(deced).toEqual("abc");
+  test("Should check for key equivalence", () => {
+    const key1 = "1abab2bc0ccd";
+    const key2 = "1ab2c0ddbbd";
+    expect(new PolybiusCipher(key1, true).isEquivalentKey(key2)).toBe(true);
   });
 });
--- src/Ciphers/Polybius/index.ts
+++ src/Ciphers/Polybius/index.ts
@@ -1,42 +1,84 @@
 import { isAlpha, isAlphaNum } from "../../Utils/other";
 import { div } from "../../Utils/math";
 import { KeyCipher } from "../../AbstractCiphers";
+import StringBilder from "../../Utils/StringBuilder";
 
-type TCipherChars = Array<string>;
+type TWorkKeyMap = Map<string, string>;
+type TCipherChars = Array<string> | string;
+type TPrepareKeyTest = (string: string) => boolean;
+const isTCipherChars = (v: any): v is TCipherChars => {
+  return typeof v == "string" || Array.isArray(v);
+};
 
+/**
+ * An implementation of Polybius square cipher
+ */
 export class PolybiusCipher extends KeyCipher {
+  private size: 5 | 6;
   private userKey: string;
-  private workKey: Map<string, string>;
-  private cipherChars: TCipherChars;
-  constructor(key: string, cipherChars?: TCipherChars) {
+  private extended: boolean;
+  private workKey!: TWorkKeyMap;
+  private cipherChars!: TCipherChars;
+  private prepareKeyTest: TPrepareKeyTest;
+  constructor(key: string);
+  constructor(key: string, cipherChars: TCipherChars);
+  constructor(key: string, extended: boolean, cipherChars?: TCipherChars);
+  constructor(key: any, arg0?: any, arg1?: any) {
     super();
-    PolybiusCipher.checkKey(key);
-    this.userKey = key;
-
-    if (
-      cipherChars === null ||
-      cipherChars === undefined ||
-      !Array.isArray(cipherChars) ||
-      cipherChars.length < 5
-    ) {
-      this.cipherChars = PolybiusCipher.defaultCipherChars(false);
+    if (key == undefined || key == null) {
+      throw new Error("You have to specify a key!");
+    } else if (typeof key !== "string") {
+      throw new Error("Key has to be of type string!");
     } else {
-      this.cipherChars = cipherChars.slice(0, 5 || 6);
+      //This is when only key is provided  or when 2nd argument is cipher chars
+      if (arg0 == null || arg0 == undefined || isTCipherChars(arg0)) {
+        this.extended = false;
+        this.size = 5;
+        this.checkKey(key);
+        this.userKey = key;
+        this.setCipherChars(arg0);
+      } else {
+        //This is in case if only 1st 2nd argument is specified
+        //Or when both 2nd and 3rd are
+        //Minorities are handleled by checkKey and  setCipher chars
+        this.extended = arg0;
+        this.size = arg0 ? 6 : 5;
+        this.checkKey(key);
+        this.userKey = key;
+        this.setCipherChars(arg1);
+      }
+      this.prepareKeyTest = this.getPrepareKeyTest();
+      this.prepareKey(key.toLowerCase());
     }
-    this.workKey = this.prepareKey(key.toLowerCase());
   }
 
-  private static defaultCipherChars(extended: boolean) {
-    if (extended) {
-      return ["A", "B", "C", "D", "E"];
+  private defaultCipherChars() {
+    return Array.from("ABCDEF").slice(0, this.size);
+  }
+
+  private checkKey(key: string) {
+    if (this.size == 5) {
+      if (!isAlpha(key)) {
+        throw new Error("Key can contain only charachters!");
+      }
     } else {
-      return ["A", "B", "C", "D", "E", "F"];
+      if (!isAlphaNum(key)) {
+        throw new Error("Key can only contain numbers and charachters!");
+      }
     }
   }
 
-  private static checkKey(key: string) {
-    if (!isAlpha(key)) {
-      throw new Error("Key can only contain charachters");
+  private setCipherChars(cipherChars: any) {
+    if (cipherChars === null || cipherChars === undefined) {
+      this.cipherChars = this.defaultCipherChars();
+    } else if (!isTCipherChars(cipherChars)) {
+      throw new Error("Cipher chars must be an array or string!");
+    } else if (cipherChars.length < this.size) {
+      throw new Error(
+        `Cipher chars for size ${this.size}x${this.size} me at least ${this.size} long`
+      );
+    } else {
+      this.cipherChars = cipherChars.slice(0, this.size);
     }
   }
 
@@ -45,46 +87,78 @@ export class PolybiusCipher extends KeyCipher {
   }
 
   private prepareKey(key: string) {
-    const allchars = key + "abcdefghiklmnoprstquvwxyz";
-    const map = new Map();
+    const allchars = key + this.charPool();
+    this.workKey = new Map();
 
     let p = 0;
     for (const char of allchars) {
-      const i = div(p, 5);
-      const j = p % 5;
-      if (isAlpha(char) && char != "j" && !map.has(char)) {
+      const i = div(p, this.size);
+      const j = p % this.size;
+      if (this.prepareKeyTest(char)) {
         const encByte = this.makeByte(i, j);
-        map.set(char, encByte);
-        map.set(encByte, char);
+        this.workKey.set(char, encByte);
+        this.workKey.set(encByte, char);
         ++p;
       }
     }
+  }
 
-    return map;
+  private getPrepareKeyTest(): TPrepareKeyTest {
+    if (this.extended) {
+      return (char: string) => isAlphaNum(char) && !this.workKey.has(char);
+    } else {
+      return (char: string) =>
+        isAlpha(char) && char != "j" && !this.workKey.has(char);
+    }
+  }
+
+  private charPool() {
+    return "abcdefgijklmnopqrstuvwxyz" + (this.extended ? "0123456789" : "");
   }
 
   private demap(char: string) {
     return this.workKey.get(char) || "";
   }
 
-  encrypt(text: string) {
-    const result = new Array(text.length);
+  public encrypt(text: string) {
+    const buffer = new StringBilder(2 * text.length);
     for (let i = 0; i < text.length; ++i) {
-      result[i] = this.demap(text[i].toLowerCase());
+      buffer.append(this.demap(text[i].toLowerCase()));
+      buffer.append(" ");
     }
-    return result.join("");
+    return buffer.toString();
   }
 
-  decrypt(text: string) {
-    const result = new Array(div(text.length, 2));
-    for (let i = 0; i < text.length / 2; ++i) {
-      result[i] = this.demap(text.substr(2 * i, 2));
+  public decrypt(text: string, isSpaced = true) {
+    const increment = isSpaced ? 3 : 2;
+    const count = div(text.length + 1, increment);
+    const buffer = new StringBilder(count);
+    for (let i = 0; i < text.length; i += increment) {
+      buffer.append(this.demap(text[i] + text[i + 1]));
     }
-
-    return result.join("");
+    return buffer.toString();
   }
 
   isEquivalentKey(key: any): boolean {
-    throw new Error("Method not implemented.");
+    const sbMain = new StringBilder(40);
+    const sbTest = new StringBilder(40);
+    const setMain = new Set();
+    const setTest = new Set();
+    const maxSize = this.extended ? 36 : 25;
+    const fullKeyMain = (this.userKey + this.charPool()).toLowerCase();
+    const fullKeyTest = (key + this.charPool()).toLowerCase();
+    for (let i = 0; i < fullKeyMain.length; ++i) {
+      if (setMain.has(fullKeyMain[i])) continue;
+      else setMain.add(fullKeyMain[i]);
+      sbMain.append(fullKeyMain[i]);
+      if (setMain.size == maxSize) break;
+    }
+    for (let i = 0; i < fullKeyTest.length; ++i) {
+      if (setMain.has(fullKeyTest[i])) continue;
+      else setMain.add(fullKeyTest[i]);
+      sbTest.append(fullKeyTest[i]);
+      if (setTest.size == maxSize) break;
+    }
+    return sbTest.toString() == sbTest.toString();
   }
 }
